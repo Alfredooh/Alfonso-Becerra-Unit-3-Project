690f3463cb4cc550caeeecf85b8a161f4b349778
ALFONSO BECERRA <126791751+Alfredooh@users.noreply.github.com>
2023-03-02 14:31:52 +0000

    Unit 3 Project

---
+++ script.js
@@ -0,0 +1,32 @@
+let storyopening = document.querySelector(".story-opening");
+let optionOneScreen = document.querySelector(".option-one-screen");
+let optionTwoScreen = document.querySelector(".option-two-screen");
+let optionOneEnd = document.querySelector(".option-one-end");
+let optionTwoEnd = document.querySelector(".option-two-end");
+let Choice1 = document.querySelector(".Choice1");
+let Choice2 = document.querySelector(".Choice2");
+
+Choice1.onclick=function(){
+  optionOneScreen.style.display="block";
+};
+
+Choice2.onclick=function(){
+  optionTwoScreen.style.display="block";
+};
+
+
+/* When you're ready to make event handlers, uncomment the code below. Then fill in the blanks with the correct variables!
+INSERT_VARIABLE.onclick=function(){
+
+};
+
+INSERT_VARIABLE.onclick=function(){
+
+};
+
+
+INSERT_VARIABLE.onclick=function(){
+
+};
+
+*/
